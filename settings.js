a90a183e7146b9e25b4f699f05e38037f2f3c1b7
YUDAMODS <abdulsamid317@gmail.com>
2024-04-02 21:31:18 +0700

    github

---
+++ settings.js
@@ -0,0 +1,34 @@
+/*
+• Base Ori YudaMods
+Penting ‼️
+
+Hapus Bagian/Teks Ini? Masuk Neraka Paling Bawah
+
+Script Ini Murni Bikinan Sendiri, Saya Hanya Sekedar Kroco Penghuni Inti Bumi.
+
+Thanks To :                                
+- Allah Swt 
+- Nabi Muhammad Saw         
+- My Parents       
+- YudaMods [ Develover Sc ]  
+- Pengguna Bot Yang Selalu Support
+
+• Script By ( YudaMods )
+*/
+
+const settings = {
+  token: '6916243118:AAGQFFvrRnOEP57pu0-A4REiI6baja2OIzw', // Token Bot
+  adminId: '5887546516',
+  urladmin: 'https://t.me/YUDAMODS',
+  pp: 'https://telegra.ph/file/7f99008ffefc5bc05c5c7.jpg',
+    //SERVER 1
+  domain: 'https://ahmadfauzi.panelprivv.xyz', // Isi dengan domain yang digunakan
+  plta: 'ptla_lPoWVONI7TdYAJD88pe9e6mNEVmnclwhis1BW3VuoQ1', // Isi dengan nilai plta yang sesuai
+  pltc: 'ptlc_TbxpWbdzlgjZrYsLJigph1mTe8ArZWdnX2zpgfcXHPQ', // Isi dengan nilai pltc yang sesuai
+  
+  //CREATE PANEL
+  loc: '1', // Isi dengan lokasi yang diinginkan
+  eggs: '15'
+};
+
+module.exports = settings;
